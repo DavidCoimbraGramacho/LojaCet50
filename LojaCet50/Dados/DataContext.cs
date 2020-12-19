7ad30cdf06e9af37277a9a2ef3f7a88b3bb3356d
David Gramacho <David.Coimbra.Gramacho@formandos.cinel.pt>
2020-12-19 00:32:21 +0000

    create database and table products

---
+++ LojaCet50/Dados/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using LojaCet50.Dados.Entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace LojaCet50.Dados
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+    }
+}
--- LojaCet50/Startup.cs
+++ LojaCet50/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LojaCet50.Dados;
+using Microsoft.EntityFrameworkCore;
 
 namespace LojaCet50
 {
@@ -24,6 +26,18 @@ namespace LojaCet50
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+
+
+            services.AddDbContext<DataContext>(cfg =>
+            {
+                cfg.UseSqlServer(this.Configuration.GetConnectionString("DefaultConnection"));
+ 
+            });
+
+
+
+
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
